68a22375b8d51f67bcac04c92cfa29558ff576e6
malina <dmk9076@gmail.com>
2023-02-14 16:21:23 +0300

    Homework#1 and #2 complite

--- CSharpHomeworks/HomeworkFromSeminar12.02.23/Task3/Program.cs
+++ CSharpHomeworks/HomeworkFromSeminar12.02.23/Task3/Program.cs
@@ -5,3 +5,10 @@
 
 6 -> да 7 -> да 1 -> нет
 */
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int nDay = int.Parse(Console.ReadLine());//строка в число
+if (nDay>0&&nDay<6)Console.WriteLine($"{nDay}-> нет");
+else if (nDay>5&&nDay<8)Console.WriteLine($"{nDay}-> да");
+else Console.WriteLine($"Ошибка выбора дня недели");
