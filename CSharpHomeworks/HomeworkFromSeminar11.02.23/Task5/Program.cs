588ee6f438f3fb4c4aa632de3338c522fbec4f24
malina <dmk9076@gmail.com>
2023-02-14 16:47:21 +0300

    Fogotten task added

---
+++ CSharpHomeworks/HomeworkFromSeminar11.02.23/Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+7. Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+
+782 -> 2
+
+918 -> 8
+*/
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+while (a <-999 || (a >-100 && a<100) || a >999)
+{
+Console.WriteLine("Введите трехзначное число:");
+a = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"{a} -> {Math.Abs(a%10)}");
